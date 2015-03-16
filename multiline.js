fb3fdc97ef13c32edab941275293cc4e87687a71
paper <jue_paper@qq.com>
2015-03-16 10:33:15 +0800

    add seajs version

--- multiline.js
+++ multiline.js
@@ -1,22 +1,22 @@
 /**
   from https://github.com/paper/javascript-multiline-string
  */
+;(function (root) {
  
-define(function(require, exports, module) {
-  
   function trim(s){
     return s.replace(/(^\s*)|(\s*$)/g, "");
   }
-  
+
   // 提取注释里面的字符串
   function getString(comments) {
-    return comments.slice(2, -2);
+   return comments.slice(2, -2);
   }
-  
+
   // 提取注释里面的字符串，并解析
   function getStringByObject(obj, comments) {
-    var s = getString(comments);
-    return s.replace(/\{\{([^\{\}]+)\}\}/g, function (a, b) {
+   var s = getString(comments);
+   
+   return s.replace(/\{\{([^\{\}]+)\}\}/g, function (a, b) {
       
       if (b) {
         
@@ -35,40 +35,52 @@ define(function(require, exports, module) {
         
       }
     });
+    
   }
-  
-  var multiline = function (obj, func) {
-    if (arguments.length == 1) {
-      func = obj;
-    }
-    var funcString = func.toString(),
-    temp = [],
-    ret = [],
-    i = 0,
-    len = 0;
-    // 取出所有的注释
-    funcString.replace(/\/\*([\s\S])*?\*\//g, function (a) {
-      if (a) {
-        temp.push(a);
-      }
-    });
-    if (temp.length === 0)
-      return '';
-    len = temp.length;
-    if (arguments.length == 1) {
-      for (; i < len; i++) {
-        ret[i] = getString(temp[i]);
-      }
-    } else {
-      for (; i < len; i++) {
-        ret[i] = getStringByObject(obj, temp[i]);
-      }
-    }
-    // 返回的是解析之后的数组
-    return ret.length === 1 ? ret[0] : ret;
-  };
-  
-  module.exports = multiline;
 
-});
+  var multiline = function (obj, func) {
+   if (arguments.length == 1) {
+     func = obj;
+   }
+   
+   var funcString = func.toString(),
+   temp = [],
+   ret = [],
+   i = 0,
+   len = 0;
+   
+   // 取出所有的注释
+   funcString.replace(/\/\*([\s\S])*?\*\//g, function (a) {
+     if (a) {
+       temp.push(a);
+     }
+   });
+   
+   if (temp.length === 0)
+     return '';
+   
+   len = temp.length;
+   
+   if (arguments.length == 1) {
+     for (; i < len; i++) {
+       ret[i] = getString(temp[i]);
+     }
+   } else {
+     for (; i < len; i++) {
+       ret[i] = getStringByObject(obj, temp[i]);
+     }
+   }
+   
+   // 返回的是解析之后的数组
+   return ret.length === 1 ? ret[0] : ret;
+  };
+
+  // AMD
+  if (typeof exports === 'object') {
+   module.exports = multiline;
+  } else {
+   root.multiline = multiline;
+  }
+ 
+})(this);
 
